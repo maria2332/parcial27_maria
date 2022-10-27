6931182ebb41634876fe66e8304ba6c8ad22b812
maria2332 <maria.ballesteros2004@gmail.com>
2022-10-27 12:01:42 +0200

    cambio

--- ejercicio7.py
+++ ejercicio7.py
@@ -3,15 +3,13 @@
 #Imprimr el número en caso de que sea múltiplo de 10 y menor que 200
 
 def recorrido(lista):
-    lista = []
     for i in lista:
-        if i % 10 == 0 and i <200:
-            lista.append(i)
-    print(lista)
+        if i %10 == 0 and i < 200:
+            print(i)
+        elif i > 300:
+            break
         
-
 lista = [18, 50, 210, 80, 145, 333, 70, 30]
 recorrido(lista)
-
 
 
