096babffef138a7fe9e7968b88e54ddbc0094f23
maria2332 <maria.ballesteros2004@gmail.com>
2022-10-27 10:56:47 +0200

    ejercicio6

---
+++ ejercicio6.py
@@ -0,0 +1,30 @@
+lista = [17, 24, 2, 13, 5, 5, 5, 13, 68]
+print("Esta es la lista original: ", lista)
+
+def modificar():
+  lista1=list(set(lista))
+  print("Lista sin elementos duplicados:", lista1)
+
+  lista1=sorted(lista1, reverse=True)
+  print("Lista ordenada de mayor a menor:", lista1)
+  
+  lista2=[]
+  for n in lista1:
+    if n % 2 == 0:
+      lista2.append
+    else:
+      pass
+
+  print("Esta es la lista sin impares:" +     str(lista2))
+
+  sumalista2= sum(lista2)
+  print("La suma de los elementos de la lista que quedan:",           sumalista2)
+
+  lista2.append(sumalista2)
+  lista2=sorted(lista2, reverse=True)
+
+  print("Comprobacion de la suma de todos los números a partir del segundo, concuerda con el primer número de la lista:",lista2)
+
+  print("",lista2[0]==sum(lista2[1:]),"")
+
+modificar()
