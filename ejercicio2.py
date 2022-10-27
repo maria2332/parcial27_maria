1b4956bb063b6ab32eb45725c973996e45c5cd16
maria2332 <maria.ballesteros2004@gmail.com>
2022-10-27 11:12:26 +0200

    cambio

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,5 +2,5 @@ def orden(cadena):
     cambio= cadena[::-1].split(",")
     print (cambio[1] + " ha sacado un " + cambio[0] + " de nota")
 
-cadena = cadena = "zeréP nauJ,01"
+cadena = "zeréP nauJ,01"
 orden(cadena)
